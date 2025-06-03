a654c3bf29345357ff850bda6bf480a7dad28565
Rip-per-Doc <warrenduffield724@gmail.com>
2025-06-03 10:50:43 +0000

    commit

---
+++ Face Quest/sketch.js
@@ -0,0 +1,21 @@
+let player;
+let bullet, explosion;
+
+function setup() {
+	new Canvas(500, 500);
+
+	player = new Sprite();
+}
+
+function preload(){
+
+}
+
+function move(){
+	if
+}
+
+function draw() {
+	background('black');
+
+}
